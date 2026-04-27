243025d64a328e11c3274180de84a65d87a73933
RafaelM <dark-dreamn@hotmail.com>
2026-04-27 10:21:50 -0300

    iniciando projeto

---
+++ src/main/java/com/rpg/rpg_battle/RpgBattleApplication.java
@@ -0,0 +1,14 @@
+package com.rpg.rpg_battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgBattleApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(RpgBattleApplication.class, args);
+        System.out.println("start");
+	}
+
+}
---
+++ src/test/java/com/rpg/rpg_battle/RpgBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.rpg_battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
