33ccee14c7ba65f1072bfd81cd3309ec42931c9d
RafaelM <dark-dreamn@hotmail.com>
2026-04-29 22:34:50 -0300

    ajustes em Characters e classe Paladin

--- src/main/java/com/rpg/rpg_battle/model/Characters.java
+++ src/main/java/com/rpg/rpg_battle/model/Characters.java
@@ -1,12 +1,18 @@
 package com.rpg.rpg_battle.model;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Random;
 
 @Getter
 @Setter
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class Characters {
+    protected static final Random RANDOM_GENERATOR = new Random();
+
     @Setter(AccessLevel.NONE)
     protected String name;
 
--- src/main/java/com/rpg/rpg_battle/model/Paladin.java
+++ src/main/java/com/rpg/rpg_battle/model/Paladin.java
@@ -1,4 +1,30 @@
 package com.rpg.rpg_battle.model;
 
-public class Paladin {
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class Paladin extends Characters {
+    @Override
+    public int usePhysicalAttack() {
+        int bonus = 0;
+        String mensagem = "Ataque de espada";
+
+        if (healthPoints < 7) {
+            bonus = 1;
+            mensagem += " +1 (Frenesi!)";
+        }
+        System.out.println(mensagem);
+        return physicalAttack + bonus;
+    }
+    @Override
+    public int useMagicPower() {
+        magicPoints = 0;
+        int power = RANDOM_GENERATOR.nextInt(3);
+        System.out.println("Espada da Aurora! + " + power);
+        return magicAttack + power;
+    }
 }
